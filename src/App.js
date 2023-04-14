1bd0c61e115d7d05ec93b88e6343a9a491a5bd42
Lameck Otieno <lameck72@gmail.com>
2023-04-15 00:59:35 +0300

    Add whole codebase

--- src/App.js
+++ src/App.js
@@ -1,11 +1,29 @@
 import React from 'react';
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from 'react-router-dom';
+import { Provider } from 'react-redux';
+import Home from './components/home/Home';
+import Details from './components/details/Details';
+import store from './redux/configureStore';
+import './App.css';
 
-function App() {
-  return (
-    <div>
-      <h1>Hello React</h1>
-    </div>
-  );
-}
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Home />,
+  },
+  {
+    path: '/coins/:id',
+    element: <Details />,
+  },
+]);
+
+const App = () => (
+  <Provider store={store}>
+    <RouterProvider router={router} />
+  </Provider>
+);
 
 export default App;
